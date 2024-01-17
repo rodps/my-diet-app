5583587bee276d50414a193291834e79a6cb8ede
Rodrigo da Silva <rodrigopds73@gmail.com>
2024-01-17 20:29:27 -0300

    add project dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,23 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from 'elysia'
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+
+app.post('/users', ({ body: { name, email, password } }) => {
+  return {
+    name,
+    email,
+    password
+  }
+}, {
+  body: t.Object({
+    name: t.String({ minLength: 1 }),
+    email: t.String({ format: 'email' }),
+    password: t.String({ minLength: 3 })
+  })
+})
+
+app.listen(3000)
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+)
